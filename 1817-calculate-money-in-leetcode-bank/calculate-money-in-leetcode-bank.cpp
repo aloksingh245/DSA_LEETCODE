c9af4307283dda4ae07d1ee452bfd6e49a3916d0
aloksingh245 <108326942+aloksingh245@users.noreply.github.com>
2025-10-31 12:19:04 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (90.52%) - LeetSync

---
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int totalMoney(int n) {
+        int sum = 0;
+        int week = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (i % 7 == 0) week++;  
+            sum += week + (i % 7);   
+        }
+        return sum;
+    }
+};
