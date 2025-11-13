2aaf85d0e6935c8c5b04449dc0e7ae348a3156d4
aloksingh245 <108326942+aloksingh245@users.noreply.github.com>
2025-11-13 15:15:45 +0530

    Time: 8 ms (80.25%) | Memory: 11.7 MB (96.29%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,22 +9,27 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if (head == NULL || head->next == NULL) {
+        if(head==NULL || head->next==NULL){
             return false;
         }
 
-        ListNode *slowptr = head;
-        ListNode *fastptr = head;
 
-        while (fastptr->next != NULL && fastptr->next->next != NULL) {
-            slowptr = slowptr->next;
-            fastptr = fastptr->next->next;
-
-            if (slowptr == fastptr) {
+        ListNode *slowptr=head;
+       ListNode *fastptr=head;
+        while(fastptr!=NULL && fastptr->next!=NULL){
+            slowptr=slowptr->next;
+            fastptr=fastptr->next->next;
+            if(slowptr==fastptr){
                 return true;
+            
             }
-        }
+            
 
+
+        }
         return false;
+
+
+        
     }
 };
