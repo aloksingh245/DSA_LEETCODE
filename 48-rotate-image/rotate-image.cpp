58851abee82e017a1886ae2f783f5a6023070563
aloksingh245 <108326942+aloksingh245@users.noreply.github.com>
2026-02-21 00:04:59 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (36.06%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int n=matrix.size();
+        int n = matrix.size();
 
-        for(int i=0;i<n/2;i++){
-            for(int j=i;j<n-i-1;j++){
-                //shiftingstart
-                //bottem left value
-                int temp=matrix[n-1-j][i];
-
-                //right bottom shift
-                matrix[n-1-j][i]=matrix[n-1-i][n-1-j];
-                //top right shift
-                matrix[n-1-i][n-1-j]=matrix[j][n-1-i];
-                //top left shift
-                matrix[j][n-1-i]=matrix[i][j];
-
-                //top value
-                matrix[i][j]= temp;
+        // Step 1: Transpose
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < n; j++) {
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
-        
-        
+
+        // Step 2: Reverse each row
+        for(int i = 0; i < n; i++) {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
     }
 };
